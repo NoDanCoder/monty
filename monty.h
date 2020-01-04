af2244a6674ae5c93f7baace3ee04fe1f00b082d
Alejandro Robayo <1249@holbertonschool.com>
2020-01-04 03:43:29 +0000

    [Add] monty.h

---
+++ monty.h
@@ -0,0 +1,61 @@
+#ifndef _MONTY_H
+#define _MONTY_H
+
+#include <stdio.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/**
+ * struct pack - transport getline and line_number
+ * @cmd: current line in operation
+ * @n: current line number
+ *
+ * Description: whole line in manipulation and his
+ * position
+ */
+struct pack
+{
+	char *cmd;
+	unsigned int n;
+	FILE *fdcode;
+} pack;
+
+void freeStack(stack_t **stack);
+int isNumber(char *str);
+void push(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void error(char *str, int shouldFree, int closeFile);
+char *str_concat(char *s1, char *s2);
+void built_in(stack_t **head);
+
+#endif /* _MONTY_H */
