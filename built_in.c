a52d05069a066d7056afa58016c2d0ff3d62b865
Alejandro Robayo <1249@holbertonschool.com>
2020-01-04 03:43:04 +0000

    [Add] built_in function to handle commands

---
+++ built_in.c
@@ -0,0 +1,39 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "monty.h"
+
+/**
+ * built_in - checks if cmd is a built in
+ * @head: stack to operate with cmd
+ *
+ * Return: On fail 0
+ */
+void built_in(stack_t **head)
+{
+	instruction_t ops[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop}
+	};
+
+	int i = sizeof(ops) / sizeof(ops[0]); /* lenght of ops array */
+	char *buffer = strtok(pack.cmd, " \n");
+
+	if (buffer == NULL)
+		return;
+
+	while (i--)
+		if (!strcmp(buffer, ops[i].opcode))
+		{
+			ops[i].f(head, pack.n);
+			return;
+		}
+
+	dprintf(2, "L%d: unknown instruction %s\n", pack.n, buffer);
+	free(pack.cmd);
+	freeStack(head);
+	fclose(pack.fdcode);
+	exit(EXIT_FAILURE);
+}
